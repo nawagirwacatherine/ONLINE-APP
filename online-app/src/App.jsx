e1ef34f6af1ed0d35a4a15f8401eb05e67c9a013
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-01-04 05:46:53 +0200

    add div for nav2

--- online-app/src/App.jsx
+++ online-app/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css"
 import Nav from "./pages/Nav.jsx"
+import Nav2 from "./pages/Nav2.jsx"
 function App () {
 return(
 <>
   <div className="app">
     < Nav/>
+    < Nav2/>
   </div>
   </>
 
--- online-app/src/pages/Nav2.jsx
+++ online-app/src/pages/Nav2.jsx
@@ -5,12 +5,11 @@ function Nav2 () {
     return(
     <>
       <div className="nav2">
-      <div className="nav2-items1">
       <div className="ticket-status">TICKET STATUS</div>
       <div>PROMO</div>
       <div>RESULTS</div>
       <div>CONTACT US</div>
-     </div>
+
     </div>
 
     </>
