94d6183f3e753fd56fdcc2e0c1ceb662fe71315a
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-01-04 05:36:28 +0200

    create component in pages

---
+++ online-app/src/pages/Nav2.jsx
@@ -0,0 +1,26 @@
+
+
+function Nav2 () {
+
+    return(
+    <>
+      <div className="nav2">
+      <div className="nav2-items1">
+      <div className="ticket-status">TICKET STATUS</div>
+      <div>PROMO</div>
+      <div>RESULTS</div>
+      <div>CONTACT US</div>
+     </div>
+    </div>
+
+    </>
+      
+
+ 
+    
+    )
+    
+    
+    }
+    
+    export default Nav2;
