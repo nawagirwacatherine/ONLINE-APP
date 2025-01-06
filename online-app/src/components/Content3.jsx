8f9274dc8103bfeecfa0a4100cf2f1d50036049a
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-01-06 07:37:56 +0200

    add divs to content 3

--- online-app/src/components/Content3.jsx
+++ online-app/src/components/Content3.jsx
@@ -5,6 +5,9 @@ function Content3 () {
      
     <div className="content3">
     <div className="betting-ticket">BETTING TICKET</div>
+    <div className="betting-ticket">  Betslip</div>
+    <div className="betting-ticket">click on odds to start betting</div>
+    <div className="betting-ticket"><button>login</button></div>
     Betslip
     </div>
       
